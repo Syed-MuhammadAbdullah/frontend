700e812f8fbe56461c9c96948d3a7c362149d749
Syed Muhammad Abdullah <152968373+Syed-MuhammadAbdullah@users.noreply.github.com>
2025-02-12 10:41:43 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-let a = document.getElementById("main")
-a.addEventListener("click",()=>document.querySelector("#hero h1").style.color="yellow")
+let a = document.getElementById("main");
+
+a.addEventListener("click", () => {
+    let heading = document.querySelector("#hero h1");
+
+    heading.innerHTML = `Welcome to My Website <br> 
+    <span id="name" style="font-size: 40px; color: yellow; opacity: 0; transform: translateY(-50px); transition: opacity 1.5s linear, transform 6s linear;">Syed Muhammad Abdullah</span>`;
+
+setTimeout(() => {
+    let name = document.getElementById("name");
+    name.style.opacity = "1";
+    name.style.transform = "translateY(0)"; // Slide from top
+}, 100);
+
+
+});
